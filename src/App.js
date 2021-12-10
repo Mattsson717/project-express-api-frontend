c69701163f4b84daab1d835cb50b254899e9f08f
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2021-12-10 11:18:19 +0100

    h

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export const App = () => {
+  const [games, setGames] = useState({});
+
+  const API_URL =
+    "https://mattssons-api.herokuapp.com/games?platform=SNES&genre=role";
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((json) => setGames(json));
+  }, []);
+
+  console.log(games);
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <>
+      <div className="fade"></div>
+      <section className="star-wars">
+        <div className="crawl">
+          <div className="title">
+            <p>Top ranked</p>
+            <h1>Super NES games</h1>
+          </div>
+          {games.map((game) => (
+            <div key={game.Rank}>
+              <p>
+                {game.name} Publisher: {game.publisher}
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
