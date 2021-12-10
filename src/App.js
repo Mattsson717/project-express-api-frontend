b1569237c3330437ecbc894f2f2321199db88845
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2021-12-10 11:36:24 +0100

    added map function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 export const App = () => {
-  const [games, setGames] = useState({});
+  const [games, setGames] = useState([]);
 
   const API_URL =
     "https://mattssons-api.herokuapp.com/games?platform=SNES&genre=role";
@@ -9,24 +9,23 @@ export const App = () => {
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
-      .then((json) => setGames(json));
+      .then((json) => setGames(json.response));
   }, []);
 
   console.log(games);
   return (
     <>
-      <div className="fade"></div>
+      <div className="fade" />
       <section className="star-wars">
         <div className="crawl">
           <div className="title">
             <p>Top ranked</p>
-            <h1>Super NES games</h1>
+            <h1>SNES roleplaying games</h1>
           </div>
           {games.map((game) => (
             <div key={game.Rank}>
-              <p>
-                {game.name} Publisher: {game.publisher}
-              </p>
+              <p>{game.Name}</p>
+              <p>Publisher: {game.Publisher}</p>
             </div>
           ))}
         </div>
